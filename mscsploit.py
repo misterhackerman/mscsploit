f43ff17ef51a1b310d9e06261b67709e588d0862
zhackk <110896434+zhackk@users.noreply.github.com>
2022-12-07 14:30:48 +0300

    Update mscsploit.py
    Fixed bug that crashes program if file names have apostrophe.

--- mscsploit.py
+++ mscsploit.py
@@ -64,7 +64,10 @@ def find_courses(url):
     return courses
 
 def find_subject_folder(name, doc):
-    if '&amp;' in name:
+    if '&#39;' not in name:
+        name = html.unescape(name)
+    else:
+        name = name.strip('&#39;')
         name = html.unescape(name)
     folder_source = doc.find_all("a", string=name)[0].parent.parent.parent.parent.parent.parent.parent.parent.parent.parent.parent.parent.parent.parent
     folder = re.findall('''</i>
@@ -96,22 +99,23 @@ def choose_course(url):
         return choose_course(url)
 
 def download_lectures(url, folder, folder_url):
+    extention = '.pdf'
     course_page = requests.get(url, headers=HEADERS)
-    links = re.findall('<a href="(.*)">.*.pdf</a>', course_page.content.decode())
-    names = re.findall('<a href=".*">(.*).pdf</a>', course_page.content.decode())
+    links = re.findall('<a href="(.*)">.*' + extention + '</a>', course_page.content.decode())
+    names = re.findall('<a href=".*">(.*)' + extention + '</a>', course_page.content.decode())
     page = requests.get(folder_url, headers=HEADERS)
     doc = BeautifulSoup(page.text, 'html.parser')
     x=0
     y=0
     prev_sub_folder = None
     for link in links:
-        link = link.strip() + '.pdf'
-        subject_folder = find_subject_folder(names[x] + '.pdf', doc)
+        link = link.strip() + extention
+        subject_folder = find_subject_folder(names[x] + extention, doc)
         if subject_folder == prev_sub_folder:
             pass
         else:
             y = 0 
-        new_name = str(y+1) + '. ' + names[x] + '.pdf'
+        new_name = str(y+1) + '. ' + names[x] + extention
         x += 1
         y += 1
         prev_sub_folder = subject_folder
