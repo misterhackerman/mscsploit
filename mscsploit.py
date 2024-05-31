b063a9d04f0522c07c305a29351d9634a0eae1ec
zhackk <110896434+zhackk@users.noreply.github.com>
2024-05-31 15:42:17 +0300

    verbosity control

--- mscsploit.py
+++ mscsploit.py
@@ -11,6 +11,7 @@ parser = argparse.ArgumentParser(description='API to download lectures off msc-m
 parser.add_argument('-t', '--category', type=int, metavar='', help='to specify category number')
 parser.add_argument('-c', '--course', type=int, metavar='', help='to specify course number')
 parser.add_argument('-f', '--folder', type=str, metavar='', help='to specify destination folder')
+parser.add_argument('-v', '--verbose', action='store_true', help='Increase Verbosity')
 args = parser.parse_args()
 
 DECOR = ' \033[34;1m::\033[0m'
@@ -181,7 +182,8 @@ def download_from_dict(path_link_dict, folder):
         counter = counter + 1
         count = f' ({counter}/{len(path_link_dict)})'
         if os.path.isfile(folder + path + name):
-            print('[ Already there! ] ' + name + count)
+            if args.verbose:
+                print('[ Already there! ] ' + name + count)
             continue
 
         if not os.path.isdir(folder + path):
@@ -214,9 +216,9 @@ def main():
 if __name__ == '__main__':
     print(''' ███╗   ███╗███████╗ ██████╗███████╗██████╗ ██╗      ██████╗ ██╗████████╗
  ████╗ ████║██╔════╝██╔════╝██╔════╝██╔══██╗██║     ██╔═══██╗██║╚══██╔══╝
- ██╔████╔██║███████╗██║     ███████╗██████╔╝██║     ██║   ██║██║   ██║   
- ██║╚██╔╝██║╚════██║██║     ╚════██║██╔═══╝ ██║     ██║   ██║██║   ██║   
- ██║ ╚═╝ ██║███████║╚██████╗███████║██║     ███████╗╚██████╔╝██║   ██║   
+ ██╔████╔██║███████╗██║     ███████╗██████╔╝██║     ██║   ██║██║   ██║
+ ██║╚██╔╝██║╚════██║██║     ╚════██║██╔═══╝ ██║     ██║   ██║██║   ██║
+ ██║ ╚═╝ ██║███████║╚██████╗███████║██║     ███████╗╚██████╔╝██║   ██║
  ╚═╝     ╚═╝╚══════╝ ╚═════╝╚══════╝╚═╝     ╚══════╝ ╚═════╝ ╚═╝   ╚═╝''')
     try:
         main()
